76ed8d80432bffa9c0337ae0345a65e191de234d
Міцинська Єлизавета <mitsinskayae@atbmarket.com>
2023-11-18 00:48:03 +0200

    fix

--- lib/edit_screen.dart
+++ lib/edit_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:my_study/item_screen.dart';
 import 'package:my_study/provider_state_managment.dart';
 import 'package:provider/provider.dart';
 import 'data/data.dart';
